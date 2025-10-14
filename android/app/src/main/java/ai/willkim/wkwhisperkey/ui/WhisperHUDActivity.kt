4e6ce259034f663c8dfbbeda1aeaeaaecec981ac
Will Gi-il, Kim <kiexpert@kivilab.co.kr>
2025-10-14 22:20:20 +0900

    Update WhisperHUDActivity.kt

--- android/app/src/main/java/ai/willkim/wkwhisperkey/ui/WhisperHUDActivity.kt
+++ android/app/src/main/java/ai/willkim/wkwhisperkey/ui/WhisperHUDActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
