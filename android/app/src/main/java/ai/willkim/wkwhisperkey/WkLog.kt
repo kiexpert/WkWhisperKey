9d1d6dbe2a3a40c810ecd93410be09c87dbaccc3
Will Gi-il, Kim <kiexpert@kivilab.co.kr>
2025-10-15 00:30:55 +0900

    Update WkLog.kt

--- android/app/src/main/java/ai/willkim/wkwhisperkey/WkLog.kt
+++ android/app/src/main/java/ai/willkim/wkwhisperkey/WkLog.kt
@@ -1,6 +1,15 @@
+package ai.willkim.wkwhisperkey
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * Global reactive log system.
+ * Collectable by Compose UI.
+ */
 object WkLog {
     private val _logs = MutableStateFlow<List<String>>(emptyList())
-    val logs: StateFlow<List<String>> = _logs
+    val logs: StateFlow<List<String>> get() = _logs
 
     fun i(tag: String, msg: String) = add("I/$tag: $msg")
     fun e(tag: String, msg: String) = add("E/$tag: $msg")
@@ -8,5 +17,6 @@ object WkLog {
     private fun add(line: String) {
         val updated = (_logs.value + line).takeLast(300)
         _logs.value = updated
+        android.util.Log.d("WkLog", line)
     }
 }
