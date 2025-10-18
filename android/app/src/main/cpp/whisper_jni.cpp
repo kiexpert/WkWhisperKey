682224fa15cf21956667f4d29f2f1ea5e4c6afb1
Will Gi-il, Kim <kiexpert@kivilab.co.kr>
2025-10-19 03:08:57 +0900

    Create whisper_jni.cpp

---
+++ android/app/src/main/cpp/whisper_jni.cpp
@@ -0,0 +1,18 @@
+#include <jni.h>
+#include <android/log.h>
+#include "wk_safety.cpp"   // 안전 핸들러 포함
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_ai_willkim_wkwhisperkey_whisper_native_WhisperCppEngine_nativeTranscribe(
+        JNIEnv* env, jobject thiz, jobject buffer, jint length) {
+    __android_log_print(ANDROID_LOG_INFO, "WkNative", "dummy nativeTranscribe called (%d bytes)", length);
+    return env->NewStringUTF("[native stub: whisper not yet linked]");
+}
+
+/** JNI_OnLoad: 네이티브 라이브러리 로드시 자동 호출 */
+jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    __android_log_print(ANDROID_LOG_INFO, "WkNative", "JNI_OnLoad called, registering safety hooks");
+    register_native_safety();
+    return JNI_VERSION_1_6;
+}
