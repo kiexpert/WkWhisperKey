fa6bd3ebe35490b4b2e95b897c0b6c2c1e54e831
Will Gi-il, Kim <kiexpert@kivilab.co.kr>
2025-10-19 16:31:31 +0900

    Update WkMicArrayManager.kt

--- android/app/src/main/java/ai/willkim/wkwhisperkey/audio/WkMicArrayManager.kt
+++ android/app/src/main/java/ai/willkim/wkwhisperkey/audio/WkMicArrayManager.kt
@@ -1,5 +1,12 @@
 package ai.willkim.wkwhisperkey.audio
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.media.AudioManager
+import android.os.Build
+import android.widget.Toast
 import android.content.Context
 import android.media.*
 import android.util.Log
